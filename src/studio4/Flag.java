478f35e97e6f83815cb0d11cefe4d8d74942262f
sara.moss <sara.moss@10.232.194.182>
2023-10-04 13:56:21 -0500

    Complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,17 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setXscale(0,28);
+		StdDraw.setYscale(0,22);
+		StdDraw.setPenColor(0,0,0);
+	    StdDraw.rectangle(14,11,13,8.5);
+	    
+		StdDraw.setXscale(0,22);
+		StdDraw.setYscale(0,16);
+		StdDraw.setPenColor(150,123,182);
+	    StdDraw.filledRectangle(11,5,10,3);
+	    StdDraw.setPenColor(31,78,47);
+	    StdDraw.filledRectangle(11,11,10,3);
 	}
 }
